a20bca75e1fa0637ce41754bc3065c59fc9f2a23
Ruslan Mikhalev <RMikhalev@alfabank.ru>
2019-05-31 12:01:19 +0300

    Simple template

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void test1() {
+        // тест 1
+    }
+
+    @Test
+    public void test2() {
+        // тест 2
+    }
+}
